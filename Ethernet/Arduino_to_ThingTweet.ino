4d334cea1ba3b4d5a0109d2e1769636f9b1bd43f
Bongjun Hur <bjnhur@gmail.com>
2015-06-29 21:17:03 +0900

    Update file to add new IDE & W5500 Ethernet Shield

--- Ethernet/Arduino_to_ThingTweet.ino
+++ Ethernet/Arduino_to_ThingTweet.ino
@@ -31,7 +31,10 @@
  Additional Credits:
  Example sketches from Arduino team, Ethernet by Adrian McEwen
  
+ Updated: June 29, 2015 by Bongjun Hur
+ changed some code for new WIZnet W5500 Ethernet Shield.
 */
+
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -39,9 +42,16 @@
 // Local Network Settings
 byte mac[] = { 0xD4, 0x28, 0xB2, 0xFF, 0xA0, 0xA1 }; // Must be unique on local network
 
+// for static IP users.
+IPAddress ip(222, 98, 173, 232);
+IPAddress gateway(222, 98, 173, 254);
+IPAddress subnet(255, 255, 255, 192);
+// fill in your Domain Name Server address here:
+IPAddress myDns(8, 8, 8, 8); // google puble dns
+
 // ThingSpeak Settings
 char thingSpeakAddress[] = "api.thingspeak.com";
-String thingtweetAPIKey = "XXXMX2WYYR0EV68M";
+String thingtweetAPIKey = "V4VFDWX6LNWOU08H";
 
 // Variable Setup
 long lastConnectionTime = 0; 
@@ -51,10 +61,17 @@ int failedCounter = 0;
 // Initialize Arduino Ethernet Client
 EthernetClient client;
 
+// error at 1.6.4 IDE, so add declaration of function. 
+void startEthernet();
+void updateTwitterStatus(String tsData);
+
 void setup()
 {
-  // Start Serial for debugging on the Serial Monitor
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
   
   // Start Ethernet on Arduino
   startEthernet();
@@ -62,7 +79,7 @@ void setup()
   delay(1000);
   
   // Update Twitter via ThingTweet
-  updateTwitterStatus("My thing is social @thingspeak");
+  updateTwitterStatus("My thing is social #3 @thingspeak");
 }
 
 void loop()
@@ -146,7 +163,13 @@ void startEthernet()
   delay(1000);
   
   // Connect to network amd obtain an IP address using DHCP
+  // initialize the ethernet device
+#if defined __USE_DHCP__
+#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
+  if (Ethernet.begin() == 0)
+#else
   if (Ethernet.begin(mac) == 0)
+#endif  
   {
     Serial.println("DHCP Failed, reset Arduino to try again");
     Serial.println();
@@ -156,6 +179,15 @@ void startEthernet()
     Serial.println("Arduino connected to network using DHCP");
     Serial.println();
   }
-  
+#else
+#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
+  Ethernet.begin(ip, myDns, gateway, subnet);
+#else
+  Ethernet.begin(mac, ip, myDns, gateway, subnet);
+#endif  
+    Serial.println("Arduino connected to network using Static IP : ");
+    Serial.println(Ethernet.localIP());
+#endif   
+
   delay(1000);
 }
