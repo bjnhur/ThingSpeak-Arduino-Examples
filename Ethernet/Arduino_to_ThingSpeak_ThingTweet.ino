5b1b82ec09bb8118f2756ed74d2b4aab92702ecd
Bongjun Hur <bjnhur@gmail.com>
2015-06-30 10:34:07 +0900

    Update and rename Arduino_to_ThingSpeak.ino to Arduino_to_ThingSpeak_ThingTweet.ino

---
+++ Ethernet/Arduino_to_ThingSpeak_ThingTweet.ino
@@ -0,0 +1,251 @@
+/*
+
+ Arduino --> ThingSpeak Channel via Ethernet
+
+ The ThingSpeak Client sketch is designed for the Arduino and Ethernet.
+ This sketch updates a channel feed with an analog input reading via the
+ ThingSpeak API (https://thingspeak.com/docs)
+ using HTTP POST. The Arduino uses DHCP and DNS for a simpler network setup.
+ The sketch also includes a Watchdog / Reset function to make sure the
+ Arduino stays connected and/or regains connectivity after a network outage.
+ Use the Serial Monitor on the Arduino IDE to see verbose network feedback
+ and ThingSpeak connectivity status.
+
+ Getting Started with ThingSpeak:
+
+   * Sign Up for New User Account - https://thingspeak.com/users/new
+   * Create a new Channel by selecting Channels and then Create New Channel
+   * Enter the Write API Key in this sketch under "ThingSpeak Settings"
+
+ Arduino Requirements:
+
+   * Arduino with Ethernet Shield or Arduino Ethernet
+   * Arduino 1.0+ IDE
+
+  Network Requirements:
+
+   * Ethernet port on Router
+   * DHCP enabled on Router
+   * Unique MAC Address for Arduino
+
+ Created: October 17, 2011 by Hans Scharler (http://www.nothans.com)
+
+ Additional Credits:
+ Example sketches from Arduino team, Ethernet by Adrian McEwen
+
+ Updated: June 29, 2015 by Bongjun Hur
+ changed some code for new WIZnet W5500 Ethernet Shield.
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Local Network Settings
+byte mac[] = { 0xD4, 0x28, 0xB2, 0xFF, 0xA0, 0xA1 }; // Must be unique on local network
+
+// for static IP users.
+IPAddress ip(222, 98, 173, 232);
+IPAddress gateway(222, 98, 173, 254);
+IPAddress subnet(255, 255, 255, 192);
+// fill in your Domain Name Server address here:
+IPAddress myDns(8, 8, 8, 8); // google puble dns
+
+// ThingSpeak Settings
+char thingSpeakAddress[] = "api.thingspeak.com";
+String thingtweetAPIKey = "V4VFDWX6LNWOU08H";
+String writeAPIKey = "9H6UUYP84BV5FYJ8";
+const int updateThingSpeakInterval = 16 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+
+// Variable Setup
+long lastConnectionTime = 0;
+boolean lastConnected = false;
+int failedCounter = 0;
+
+// Initialize Arduino Ethernet Client
+EthernetClient client;
+
+// error at 1.6.4 IDE, so add declaration of function.
+void startEthernet();
+void updateThingSpeak(String tsData);
+void updateTwitterStatus(String tsData);
+
+void setup()
+{
+    // Start Serial for debugging on the Serial Monitor
+    Serial.begin(9600);
+
+    // Start Ethernet on Arduino
+    startEthernet();
+}
+
+void loop()
+{
+    // Read value from Analog Input Pin 0
+    int photocellReading; // the analog reading from the analog resistor divider
+    photocellReading = analogRead(A0);
+    String analogValue0 = String(photocellReading, DEC);
+
+    // Print Update Response to Serial Monitor
+    if (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+    }
+
+    // Disconnect from ThingSpeak
+    if (!client.connected() && lastConnected) {
+        Serial.println("...disconnected");
+        Serial.println();
+        client.stop();
+    }
+
+    // Update ThingSpeak
+    if(!client.connected() && (millis() - lastConnectionTime > updateThingSpeakInterval)) {
+        Serial.println("field1="+analogValue0);
+        updateThingSpeak("field1="+analogValue0);
+        // Update Twitter via ThingTweet
+        String tweetmsg = "My Simple IoT says, now my office is ";
+        if (photocellReading < 10) {
+            Serial.println(" - Dark");
+            tweetmsg += "Dark.";
+        } else if (photocellReading < 200) {
+            Serial.println(" - Dim");
+            tweetmsg += "Dim.";
+        } else if (photocellReading < 500) {
+            Serial.println(" - Light");
+            tweetmsg += "Light.";
+        } else if (photocellReading < 800) {
+            Serial.println(" - Bright");
+            tweetmsg += "Bright.";
+        } else {
+            Serial.println(" - Very bright");
+            tweetmsg += "Very bright.";
+        }
+        tweetmsg += " @WiznetTeam @thingspeak";
+        updateTwitterStatus(tweetmsg);
+
+    }
+
+    // Check if Arduino Ethernet needs to be restarted
+    if (failedCounter > 3 ) {
+        startEthernet();
+    }
+
+    lastConnected = client.connected();
+}
+
+void startEthernet()
+{
+    client.stop();
+
+    Serial.println("Connecting Arduino to network...");
+    Serial.println();
+
+    delay(1000);
+
+    // Connect to network amd obtain an IP address using DHCP
+    // initialize the ethernet device
+#if defined __USE_DHCP__
+#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
+    if (Ethernet.begin() == 0)
+#else
+    if (Ethernet.begin(mac) == 0)
+#endif
+    {
+        Serial.println("DHCP Failed, reset Arduino to try again");
+        Serial.println();
+    } else {
+        Serial.println("Arduino connected to network using DHCP");
+        Serial.println();
+    }
+#else
+#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
+    Ethernet.begin(ip, myDns, gateway, subnet);
+#else
+    Ethernet.begin(mac, ip, myDns, gateway, subnet);
+#endif
+    Serial.println("Arduino connected to network using Static IP : ");
+    Serial.println(Ethernet.localIP());
+#endif
+
+    delay(1000);
+}
+
+void updateThingSpeak(String tsData)
+{
+    if (client.connect(thingSpeakAddress, 80)) {
+        client.print("POST /update HTTP/1.1\n");
+        client.print("Host: api.thingspeak.com\n");
+        client.print("Connection: close\n");
+        client.print("X-THINGSPEAKAPIKEY: "+writeAPIKey+"\n");
+        client.print("Content-Type: application/x-www-form-urlencoded\n");
+        client.print("Content-Length: ");
+        client.print(tsData.length());
+        client.print("\n\n");
+
+        client.print(tsData);
+
+        lastConnectionTime = millis();
+
+        if (client.connected()) {
+            Serial.println("Connecting to ThingSpeak...");
+            Serial.println();
+
+            failedCounter = 0;
+        } else {
+            failedCounter++;
+
+            Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");
+            Serial.println();
+        }
+
+    } else {
+        failedCounter++;
+
+        Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");
+        Serial.println();
+
+        lastConnectionTime = millis();
+    }
+}
+
+
+void updateTwitterStatus(String tsData)
+{
+    client.stop();
+    if (client.connect(thingSpeakAddress, 80)) {
+        // Create HTTP POST Data
+        tsData = "api_key="+thingtweetAPIKey+"&status="+tsData;
+
+        client.print("POST /apps/thingtweet/1/statuses/update HTTP/1.1\n");
+        client.print("Host: api.thingspeak.com\n");
+        client.print("Connection: close\n");
+        client.print("Content-Type: application/x-www-form-urlencoded\n");
+        client.print("Content-Length: ");
+        client.print(tsData.length());
+        client.print("\n\n");
+
+        client.print(tsData);
+
+        lastConnectionTime = millis();
+
+        if (client.connected()) {
+            Serial.println("Connecting to ThingSpeak...");
+            Serial.println();
+
+            failedCounter = 0;
+        } else {
+            failedCounter++;
+
+            Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");
+            Serial.println();
+        }
+
+    } else {
+        failedCounter++;
+
+        Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");
+        Serial.println();
+
+        lastConnectionTime = millis();
+    }
+}
